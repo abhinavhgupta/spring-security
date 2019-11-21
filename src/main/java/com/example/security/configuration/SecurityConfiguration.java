37a25b3fa5870e54c1bfda09356a37c6feb5bb22
Abhinav Gupta <Abhinav.Gupta>
2019-11-21 13:38:39 +0530

    Added pages & roles based authorization

--- src/main/java/com/example/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/security/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -32,5 +33,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder getPasswordEncoder(){
 		return NoOpPasswordEncoder.getInstance();
 	}
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		
+		httpSecurity.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")		
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+	
+	}
+	
 
 }
